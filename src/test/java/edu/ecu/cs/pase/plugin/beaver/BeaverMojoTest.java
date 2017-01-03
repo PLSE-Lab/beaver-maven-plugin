17fd88c44282aeaa574ac53c07b0d051d158e536
Mark Hills <mhills@acm.org>
2017-01-03 11:37:06 -0500

    Added readme and license, updated package name to reflect new org

--- src/test/java/edu/ecu/cs/pase/plugin/beaver/BeaverMojoTest.java
+++ src/test/java/edu/ecu/cs/pase/plugin/beaver/BeaverMojoTest.java
@@ -1,11 +1,31 @@
-package edu.ecu.cs.sle.plugin.beaver;
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 ECU Program Analysis and Software Engineering Lab
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package edu.ecu.cs.pase.plugin.beaver;
 
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
-import org.apache.maven.plugin.testing.MojoRule;
 
 import org.apache.maven.plugin.testing.stubs.MavenProjectStub;
-import org.junit.Rule;
-import static org.junit.Assert.*;
 import org.junit.Test;
 
 import java.io.File;
